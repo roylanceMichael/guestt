8ea018e444997df874fe5bce29437f05f05229a6
Mike Roylance <roylance.michael@gmail.com>
2013-06-18 19:06:39 -0600

    initial commit

---
+++ guestt/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Guestt::Application.config.secret_token = '69fff1cdef59206a51fe0531bf558142971197a57d666ed47826970ba70497efa918b017d79f97d8c5a00c1d7cd5af2f74f5fb2b3f215fa49e12ab40ed914559'
