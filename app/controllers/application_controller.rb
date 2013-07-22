e975e8cb132291bea4e45096a12dd48258d22f89
Mike Roylance <roylance.michael@gmail.com>
2013-07-22 16:34:03 -0600

    latest changes with better layout

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,20 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+
+  before_filter :require_login
+
+  # From http://guides.rubyonrails.org/action_controller_overview.html
+  # and http://stackoverflow.com/questions/6209663/how-to-skip-a-before-filter-for-devises-sessionscontroller
+  def require_login
+    puts params[:controller]
+    puts "controller is set to: #{controller_name}"
+    #unless params[:controller] == 'devise/sessions'
+    unless controller_name == 'sessions' or controller_name == 'home'
+      unless user_signed_in?
+        flash[:error] = "You must be logged in to access this section"
+        redirect_to new_user_session_path # halts request cycle
+      end
+    end
+  end
+
 end
